4ffa5e18d5ca8a560d977db3e54042cb9200cde4
Tracy <tracykarnabi@hotmail.com>
2012-02-10 00:06:19 -0500

    added new files

---
+++ js/homework.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+$(document).ready(function () {
+
+	$('.skylanders img').hide();
+	$('.nav li').css("background-color", "#3399CC");
+
+	$('.tab1').on('click', function () {
+		$('.nav li').css("background-color", "#3399CC");
+		$('.tab1').css("background-color", "#66FFFF");
+		$('.skylanders img').fadeOut(500, function() {
+			$('.skylanders .zap').fadeIn();
+		});
+	});
+	
+	$('.tab2').on('click', function () {
+		$('.nav li').css("background-color", "#3399CC");
+		$('.tab2').css("background-color", "#66FFFF");
+		$('.skylanders img').fadeOut(500, function() {
+			$('.skylanders .hex').fadeIn();
+		});
+	});
+	
+	$('.tab3').on('click', function () {
+		$('.nav li').css("background-color", "#3399CC");
+		$('.tab3').css("background-color", "#66FFFF");
+		$('.skylanders img').fadeOut(500, function() {
+			$('.skylanders .whirlwind').fadeIn();
+		});
+	});
+	
+	$('.tab4').on('click', function () {
+		$('.nav li').css("background-color", "#3399CC");
+		$('.tab4').css("background-color", "#66FFFF");
+		$('.skylanders img').fadeOut(500, function() {
+			$('.skylanders .sunburn').fadeIn();
+		});
+	});
+	
+}); 
